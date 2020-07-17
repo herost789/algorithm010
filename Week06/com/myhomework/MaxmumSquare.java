851ae410fdcd0d52213f7da414217deb2e7e386c
herost <hh19880309@163.com>
2020-07-17 20:07:20 +0800

    week06 first commit

---
+++ Week06/com/myhomework/MaxmumSquare.java
@@ -0,0 +1,31 @@
+package com.myhomework;
+
+public class MaxmumSquare {
+
+    public int maximalSquare(char[][] matrix) {
+        int maxSide = 0;
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return maxSide;
+        }
+        //构造DP状态转换方程，某个点的最大值跟他所在位置的N,W,NW有关系
+        //设定初始第一行和第一列都为1
+        //状态转换方程是该位置的最大值为，N,W,NW的最小值加1
+        int rows = matrix.length, columns = matrix[0].length;
+        int[][] dp = new int[rows][columns];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (matrix[i][j] == '1') {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                    }
+                    maxSide = Math.max(maxSide, dp[i][j]);
+                }
+            }
+        }
+        int maxSquare = maxSide * maxSide;
+        return maxSquare;
+    }
+
+}
